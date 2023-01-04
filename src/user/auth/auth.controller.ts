9ce0aa642e164c2eab89fb3ae8517b3887f5dd54
Mykhailo K <kamiournew@gmail.com>
2023-01-04 01:41:57 +0100

    Complete authentication logic

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,13 +1,48 @@
+import * as bcrypt from 'bcryptjs';
 import { Body, Controller, Post } from '@nestjs/common';
-import { SignupDto } from '../dtos/auth.dto';
+import { Param } from '@nestjs/common/decorators';
+import { UnauthorizedException } from '@nestjs/common/exceptions';
+import { ParseEnumPipe } from '@nestjs/common/pipes';
+import { UserType } from '@prisma/client';
+import { GenerateProductKeyDto, SigninDto, SignupDto } from '../dtos/auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('signup')
-  signup(@Body() body: SignupDto) {
-    return this.authService.signup(body);
+  @Post('signup/:userType')
+  async signup(
+    @Body() body: SignupDto,
+    @Param('userType', new ParseEnumPipe(UserType)) userType: UserType,
+  ) {
+    if (userType !== UserType.BUYER) {
+      if (!body.productKey) {
+        throw new UnauthorizedException();
+      }
+
+      const validProductKey = `${body.email}-${userType}-${process.env.PRODUCT_KEY_SECRET}`;
+
+      const isValidProductKey = await bcrypt.compare(
+        validProductKey,
+        body.productKey,
+      );
+
+      if (!isValidProductKey) {
+        throw new UnauthorizedException();
+      }
+    }
+
+    return this.authService.signup(body, userType);
+  }
+
+  @Post('signin')
+  signin(@Body() body: SigninDto) {
+    return this.authService.signin(body);
+  }
+
+  @Post('key')
+  generateProductKey(@Body() body: GenerateProductKeyDto) {
+    return this.authService.generateProductKey(body.email, body.userType);
   }
 }
--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ConflictException } from '@nestjs/common/exceptions';
+import { ConflictException, HttpException } from '@nestjs/common/exceptions';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { User, UserType } from '@prisma/client';
 
 interface SignupParams {
   name: string;
@@ -9,11 +12,16 @@ interface SignupParams {
   password: string;
 }
 
+interface SigninParams {
+  email: string;
+  password: string;
+}
+
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
 
-  async signup(body: SignupParams) {
+  async signup(body: SignupParams, userType: UserType): Promise<string> {
     const userExists = await this.prismaService.user.findUnique({
       where: {
         email: body.email,
@@ -23,5 +31,62 @@ export class AuthService {
     if (userExists) {
       throw new ConflictException();
     }
+
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email: body.email,
+        name: body.name,
+        phone: body.phone,
+        password: hashedPassword,
+        user_type: userType,
+      },
+    });
+
+    return this.generateJWT(user);
+  }
+
+  async signin(body: SigninParams): Promise<string> {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Invalid credentials', 400);
+    }
+
+    const hashedPassword = user.password;
+
+    const isValidPassword = await bcrypt.compare(body.password, hashedPassword);
+
+    if (!isValidPassword) {
+      throw new HttpException('Invalid credentials', 400);
+    }
+
+    return this.generateJWT(user);
+  }
+
+  private generateJWT(user: User): string {
+    const token = jwt.sign(
+      {
+        name: user.name,
+        id: user.id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: '1h',
+      },
+    );
+
+    return token;
+  }
+
+  generateProductKey(email: string, userType: UserType): Promise<string> {
+    const str = `${email}-${userType}-${process.env.PRODUCT_KEY_SECRET}`;
+
+    return bcrypt.hash(str, 10);
   }
 }
